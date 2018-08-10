198559060ec76d4c75a9af0ae44ab55172a9be97
Ruslan Arhypenko <ruslanarhypenko@ruslans-macbook-air.local>
2018-08-10 17:57:25 +0200

    Initial Commit

---
+++ WatcthTy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatcthTy
+//
+//  Created by Ruslan Arhypenko on 10.08.2018.
+//  Copyright © 2018 Ruslan Arhypenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatcthTy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatcthTy
+//
+//  Created by Ruslan Arhypenko on 10.08.2018.
+//  Copyright © 2018 Ruslan Arhypenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
